a4b739cf97f7ec5262a1e6542e27cd3dadc8337c
Mohammad Amin Ahmadi <0x9n0p@gmail.com>
2024-02-28 18:22:26 +0330

    Read frames from the TUN device and call enqueue

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::arch::global_asm;
+use std::io;
+
+global_asm!(include_str!("queue.asm"));
+
+extern {
+    fn enqueue(frame: *const u8, n: u64);
+}
+
+fn main() -> io::Result<()> {
+    let device = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+
+    let mut buf = [0u8; 1504];
+    loop {
+        let n = device.recv(&mut buf[..])?;
+        eprintln!("{} {:x?}", n, &buf[..n]);
+
+        unsafe { enqueue(&buf as *const u8, n as u64); }
+    }
+}
---
+++ src/queue.asm
@@ -0,0 +1,8 @@
+.text
+.globl enqueue
+# void enqueue(frame*rdi, n*rsi)
+enqueue:
+    nop
+    nop
+    nop
+    ret
